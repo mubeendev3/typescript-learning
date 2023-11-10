18a0333e46594e35d1300fdeb25faae2a788b773
Mubeen Mehmood <mubeendev3@gmail.com>
2023-11-10 13:12:57 +0500

    How types work in TypeScript

---
+++ TypeScript/types.ts
@@ -0,0 +1,15 @@
+const date = new Date();
+console.log(date.getMonth());
+
+const person = {
+  age: 20,
+};
+
+console.log(person.age);
+
+class Colors {
+  red = "1F1F1F";
+}
+
+const color = new Colors();
+console.log(color.red);
