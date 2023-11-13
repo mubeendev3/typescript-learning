0720554f8eaa225b5edc5a440a94833c2ff01bde
Mubeen Mehmood <mubeendev3@gmail.com>
2023-11-13 11:36:55 +0500

    Case added for type annotations

--- TypeScript/annotations/variables.ts
+++ TypeScript/annotations/variables.ts
@@ -36,3 +36,33 @@ let point: { x: number; y: number } = {
 const displayNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+/* ------------------------ When to use annotations? ------------------------ */
+// 1) Function that returns any datatype
+const json = '{"x": 10, "y": 20}';
+
+// This is returning the type any
+// const coordinates = JSON.parse(json); // returning the datatype 'any'
+
+// To avoid the type any we have to add type annotations
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+console.log(typeof coordinates);
+
+// 2) When we declare variable on one line and initialize it later
+let colours: string[] = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < colours.length; i++) {
+  if (colours[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variables whose type cannot be inferred correctly
+let numbers: number[] = [-10, 20, -1, 5];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  numberAboveZero = numbers[i];
+}
